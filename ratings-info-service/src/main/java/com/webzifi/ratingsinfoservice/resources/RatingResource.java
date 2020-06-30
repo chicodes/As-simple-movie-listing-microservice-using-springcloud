06933cdb6c529948dc18d79f0c92d4892ba1d5ff
Akagha Chinaka <akaghachinaka@gmail.com>
2020-06-30 11:09:03 +0100

    worked on movie-catalog-service, movie-info-service and movie-ratings-info-service

---
+++ ratings-info-service/src/main/java/com/webzifi/ratingsinfoservice/resources/RatingResource.java
@@ -0,0 +1,36 @@
+package com.webzifi.ratingsinfoservice.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webzifi.ratingsinfoservice.model.Rating;
+import com.webzifi.ratingsinfoservice.model.UserRating;
+
+@RestController
+@RequestMapping("ratingsdata")
+public class RatingResource {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+		
+		return new Rating(movieId, 4);
+	}
+	
+	@RequestMapping("users/{userId}")
+	public UserRating getUserRating(@PathVariable("userId") String userId) {
+		
+		List<Rating> ratings  =  Arrays.asList(
+				new Rating("1234", 4),
+				new Rating("5678", 3)
+		);
+		
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratings);
+		
+		return userRating;
+	}
+}
---
+++ ratings-info-service/src/test/java/com/webzifi/ratingsinfoservice/RatingsInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webzifi.ratingsinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
